5e231030ff6e95a3e435e84b6c407c4a5e20625d
Mridul M Kumar <98074317+mridul0703@users.noreply.github.com>
2025-11-19 15:33:18 +0530

    Delete .executables directory

--- .executables/destroy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "💣 Destroying all Azure resources..."
-terraform destroy -auto-approve
-echo "✅ All resources destroyed successfully."
--- .executables/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "vm_public_ip" {
-  description = "Public IP of VM"
-  value       = azurerm_public_ip.pubip.ip_address
-}
-
-output "vm_username" {
-  value = var.admin_username
-}
